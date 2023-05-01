e9d2b7cd6cd6d46df5fc344ac60e74cdc5c96132
Dev-anish-kr <115490507+Dev-anish-kr@users.noreply.github.com>
2023-05-01 20:47:21 +0530

    redep

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://techmain-api.onrender.com',
+    baseUrl: 'https://techmain-api.netlify.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
